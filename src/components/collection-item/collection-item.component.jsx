540dcc1c86c6fd9e5627b5d5fd9dba762fac100a
gterra <gterra.eca@gmail.com>
2020-06-10 08:04:52 -0300

    Adding the ADD_ITEM action to cart reducer.

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,15 +1,28 @@
 import React from "react";
+import { connect } from "react-redux";
+import { addCartItem } from "./../../redux/cart/cart.actions";
 
 import "./collection-item.styles.scss";
+import CustomButton from "../custom-button/custom-button.component";
 
-const CollectionItem = ({ imageUrl, name, price }) => (
-  <div className="collection-item">
-    <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+const CollectionItem = ({ item, addCartItem }) => {
+  const { imageUrl, name, price } = item;
+  return (
+    <div className="collection-item">
+      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton inverted onClick={() => addCartItem(item)}>
+        Add to cart
+      </CustomButton>
     </div>
-  </div>
-);
+  );
+};
 
-export default CollectionItem;
+const mapDispatchToProps = (dispach) => ({
+  addCartItem: (item) => dispach(addCartItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
