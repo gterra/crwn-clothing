bb612b1cbe989fdcd4cd52d4785e7a19084bba41
gterra <gterra.eca@gmail.com>
2020-06-11 19:43:11 -0300

    Adding checkout-item actions to add and remove item from cart

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,17 +1,44 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import "./checkout-item.styles.scss";
+import {
+  clearItemFromCart,
+  addCartItem,
+  removeCartItem,
+} from "../../redux/cart/cart.actions";
 
-const CheckoutItem = ({ item: { name, imageUrl, price, quantity } }) => (
+const CheckoutItem = ({
+  item,
+  clearItemFromCart,
+  addCartItem,
+  removeCartItem,
+}) => (
   <div className="checkout-item">
     <div className="image-container">
-      <img src={imageUrl} alt="item" />
+      <img src={item.imageUrl} alt="item" />
     </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price}</span>
-    <span className="remove-button">&#10005;</span>
+    <span className="name">{item.name}</span>
+    <span className="quantity">
+      <div className="arrow" onClick={() => removeCartItem(item)}>
+        &#10094;
+      </div>
+      <span className="value">{item.quantity}</span>
+      <div className="arrow" onClick={() => addCartItem(item)}>
+        &#10095;
+      </div>
+    </span>
+    <span className="price">{item.price}</span>
+    <span className="remove-button" onClick={() => clearItemFromCart(item)}>
+      &#10005;
+    </span>
   </div>
 );
 
-export default CheckoutItem;
+const mapDispatchToProps = (dispach) => ({
+  clearItemFromCart: (item) => dispach(clearItemFromCart(item)),
+  addCartItem: (item) => dispach(addCartItem(item)),
+  removeCartItem: (item) => dispach(removeCartItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
