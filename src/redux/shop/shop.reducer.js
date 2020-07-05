8f8f3efcaace22bfaf112ef1729e48a3ec0c0191
gterra <gterra.eca@gmail.com>
2020-07-05 07:36:54 -0300

    Adding redux-thunk

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -2,16 +2,29 @@ import { ShopActionTypes } from "./shop.types";
 
 const INITIAL_STATE = {
   collections: null,
+  isFechting: false,
+  errorMessage: undefined,
 };
 
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case ShopActionTypes.UPDATE_COLLECTIONS:
+    case ShopActionTypes.FETCH_COLLECTIONS_START:
       return {
         ...state,
+        isFechting: true,
+      };
+    case ShopActionTypes.FETCH_COLLECTIONS_SUCCESS:
+      return {
+        ...state,
+        isFechting: false,
         collections: action.payload,
       };
-
+    case ShopActionTypes.FETCH_COLLECTIONS_FAILURE:
+      return {
+        ...state,
+        isFechting: false,
+        errorMessage: action.payload,
+      };
     default:
       return state;
   }
