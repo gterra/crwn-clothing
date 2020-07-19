3415e8c2abcffd9b2eb0d5e3337a386a05f151c5
gterra <gterra.eca@gmail.com>
2020-07-19 10:04:16 -0300

    Migrating missing styles scss to styled components.

--- src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
+++ src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-import "./sign-in-sign-up.styles.scss";
+import { SignInSignUpPageDiv } from "./sign-in-sign-up.styles";
 
 import SignIn from "./../../components/sign-in/sign-in.component";
 import SignUp from "./../../components/sign-up/sign-up.component";
 
 const SignInSignUpPage = () => (
-  <div className="sign-in-sign-up">
+  <SignInSignUpPageDiv>
     <SignIn />
     <SignUp />
-  </div>
+  </SignInSignUpPageDiv>
 );
 
 export default SignInSignUpPage;
