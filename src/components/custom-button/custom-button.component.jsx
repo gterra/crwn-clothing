bb8fbbe425f6ffd9b3e5b4d8cf477ac0221c76ad
gterra <gterra.eca@gmail.com>
2020-07-26 08:20:55 -0300

    Adding a backend server to deal with Strike API

--- src/components/custom-button/custom-button.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import CustomStyledButton from "./custom-button.styles";
-
-const CustomButton = ({ children, ...props }) => (
-  <CustomStyledButton {...props}>{children}</CustomStyledButton>
-);
-
-export default CustomButton;
